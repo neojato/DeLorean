c7a2c07eb1525814401cd6346494e0f473ee6b89
Kyle Paul <kpaul86+github@gmail.com>
2015-10-09 16:02:30 -0500

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,7 +342,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',

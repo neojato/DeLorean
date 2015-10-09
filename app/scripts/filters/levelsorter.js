2a2111f96e3e44fc4db6e270f6226918834cc7fe
Kyle Paul <kpaul86+github@gmail.com>
2015-10-09 15:44:21 -0500

    Add custom level sorter to ensure ordering of sponsorship levels

---
+++ app/scripts/filters/levelsorter.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name devfestApp.filter:levelSorter
+ * @function
+ * @description
+ * # levelSorter
+ * Filter in the devfestApp.
+ */
+angular.module('devfestApp')
+  .filter('levelSorter', function () {
+    function customOrder(item) {
+      switch(item) {
+        case 'Gold':
+          return 1;
+        case 'Silver':
+          return 2;
+        case 'Bronze':
+          return 3;
+      }
+    }
+
+    return function(items, field) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        return (customOrder(a.level) > customOrder(b.level) ? 1 : -1);
+      });
+      return filtered;
+    };
+  });
+

523e32d227cbdfe1db0ec41e4660df07c390fb2a
Kyle Paul <kpaul86+github@gmail.com>
2015-11-25 02:58:29 -0600

    Update event date to Feb 20, 2016

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -23,7 +23,7 @@ angular.module('devfestApp')
       'speakerURL'    : 'https://goo.gl/rEypu0', // link to the 'Call for Papers' form
       'sponsorURL'    : 'https://goo.gl/n4KoLu', // link to the sponsorship form
       'ticketURL'     : 'https://goo.gl/YNDSnR', // link to buy tickets
-      'eventDate'     : '2015-12-05', // ISO formatted YYYY-MM-DD (currently only supports a single day DevFest)
+      'eventDate'     : '2016-02-20', // ISO formatted YYYY-MM-DD (currently only supports a single day DevFest)
       'eventStart'    : '09:00:00', // start time
       'eventEnd'      : '16:00:00', // end time
       'sessionLength' : '2700000', // use minutes in milliseconds
